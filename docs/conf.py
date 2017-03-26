d40b861665d7a38e9b0b632500c993ff7604cc91
Jonathan Willis <jonathanallenwillis@gmail.com>
2017-03-26 15:09:00 -0400

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'aws-buildspec'
 year = '2017'
 author = 'Jonathan Willis'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.0'
+version = release = '0.1.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
